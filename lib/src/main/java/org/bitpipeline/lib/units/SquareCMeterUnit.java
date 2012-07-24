c2c9fd493079d112f27d70415ec9be1286cf6038
J. Miguel P. Tavares <migtavares@gmail.com>
2012-07-24 09:23:49 +0200

    Added license header to source files.

--- lib/src/main/java/org/bitpipeline/lib/units/SquareCMeterUnit.java
+++ lib/src/main/java/org/bitpipeline/lib/units/SquareCMeterUnit.java
@@ -1,8 +1,23 @@
+/**
+ * Copyright 2012 J. Miguel P. Tavares
+ *
+ * Licensed under the Apache License, Version 2.0 (the "License");
+ * you may not use this file except in compliance with the License.
+ * You may obtain a copy of the License at
+ *
+ *    http://www.apache.org/licenses/LICENSE-2.0
+ *
+ * Unless required by applicable law or agreed to in writing, software
+ * distributed under the License is distributed on an "AS IS" BASIS,
+ * WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+ * See the License for the specific language governing permissions and
+ * limitations under the License.
+ ***************************************************************************/
 package org.bitpipeline.lib.units;
 
 public class SquareCMeterUnit extends AbstractUnit {
 	static final private SquareCMeterUnit UNIT = new SquareCMeterUnit ();
-	
+
 	private SquareCMeterUnit() {
 		super("cm\u00b2", LengthDimension.dimension(), LengthDimension.dimension());
 	}
@@ -10,7 +25,7 @@ public class SquareCMeterUnit extends AbstractUnit {
 	static public Unit unit () {
 		return SquareCMeterUnit.UNIT;
 	}
-	
+
 	public Unit getUnit() {
 		return SquareCMeterUnit.unit ();
 	}
